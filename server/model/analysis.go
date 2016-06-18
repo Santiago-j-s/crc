cbace60cf60cfbeaa74ab7d2e87b59eaa696abc8
Santiago Santana <santana.santiago@gmail.com>
2016-06-18 08:59:29 -0300

    Ya funciona el análisis de capacidad de detección de errores
    Cambios para hacer commit:
    	modificado:    crc8_test.go
    	modificado:    server/model/analysis.go
    	nuevo archivo: server/model/analysis_test.go
    	modificado:    test/test_with_crc.go

--- server/model/analysis.go
+++ server/model/analysis.go
@@ -2,16 +2,34 @@
 package analysis
 
 import (
-    "github.com/Santiago-j-s/crc8"
+	"github.com/Santiago-j-s/crc8"
+	"github.com/Santiago-j-s/crc8/server/model/hamming"
 )
 
-func calculate(poly byte) map[int]int {
-    m := make(map[int]int)
-    tab := crc8.MakeTable(poly)
-    
-    for i := 0; i < 256; i++ {
-        m[i] = tab.Crc([]byte{byte(i)})
-    }
-    
-    return m
+
+func crcMap(poly byte) map[int]byte {
+	crc := make(map[int]byte)
+	tab := crc8.MakeTable(poly)
+
+	for i := 1; i < 256; i++ {
+		crc[i] = tab.Crc([]byte{byte(i)})
+	}
+
+	return crc
+}
+
+func HammingDistance(poly byte) (map[int]int, error) {
+	cnt := make(map[int]int)
+	crc := crcMap(poly)
+
+	v1 := []byte{byte(0), byte(0)}
+	for n := range crc {
+		v2 := []byte{byte(n), crc[n]}
+		d, err := hamming.Distance(v1, v2)
+		if err != nil {
+			return nil, err
+		}
+		cnt[d]++
+	}
+	return cnt, nil
 }
---
+++ server/model/analysis_test.go
@@ -0,0 +1,53 @@
+package analysis
+
+import (
+	"fmt"
+	"testing"
+)
+
+type data struct {
+	in  byte
+	out map[int]int
+}
+
+var testTable = []data{
+	{0xd7, map[int]int{
+		2: 0,
+		3: 0,
+		4: 0,
+		5: 24,
+		6: 44,
+		7: 40, // diferente a Koopman
+		8: 45,
+		9: 40,
+	}},
+	{0xd5, map[int]int{
+		2: 0,
+		3: 0,
+		4: 12,
+		5: 0,
+		6: 69,
+		7: 0,
+		8: 89,
+	}},
+}
+
+func TestHammingDistance(t *testing.T) {
+	for _, test := range testTable {
+		m, err := HammingDistance(test.in)
+		if err != nil {
+			t.Errorf("Error for input: %v", test.in)
+		}
+
+		b := true
+		for key, value := range test.out {
+			b = b && (value == m[key])
+		}
+
+		if !b {
+			s := fmt.Sprintf("\nExpected: %v\n", test.out)
+			s += fmt.Sprintf("Received: %v", m)
+			t.Errorf(s)
+		}
+	}
+}
